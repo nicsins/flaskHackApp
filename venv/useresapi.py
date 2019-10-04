187b439b2a51a9e15a43298857b7a918f556da8e
Youssef <kn1010az@go.minnstate.edu>
2019-10-04 12:37:40 -0500

    userapi

---
+++ venv/useresapi.py
@@ -0,0 +1,22 @@
+import requests
+import os
+
+apiKey = os.environ['APIKEY']
+
+users_URL = 'https://alpha-api.usbank.com/innovations/v1/users'
+user_IDs = []
+
+header = {'apiKey' : apiKey}
+response = requests.get(users_URL,headers = header).json()
+user_list = response['UserList']
+for user in user_list:
+    print(user)
+    user_IDs.append(user['LegalParticipantIdentifier'])
+
+print(user_IDs[0])
+data = {'LegalParticipantIdentifier': user_IDs[0]}
+
+accounts_url = 'https://alpha-api.usbank.com/innovations/v1/user/accounts'
+
+account_der = requests.post(accounts_url, headers= header, data= data).json()
+print(account_der)
