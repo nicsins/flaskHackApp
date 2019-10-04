0ddb45931f680bb3d7332c7f8b2a495435eb0ae4
nic <DominicB@email.com>
2019-10-04 14:11:48 -0500

    hack app add trackspending

---
+++ trackSpending.py
@@ -0,0 +1,23 @@
+#todo get user info
+
+# todo get  userID
+
+# todo get user account info
+
+
+
+#todo get mechid create an empty dictionary of merch id:totals
+
+
+#todo loop user account info  and merch dictionary with conditional when a transactions merch code and transaction merch code are the same add purchase amount to values purchaseTotals={merchcode:total}
+
+#todo create  variable TotalAmountSpent={k:sum(v) for k,v in purchaseTotals.items()}
+
+#todo create dictionary that shows percentage spent on each item percentSpent={merchcode:percent%}
+
+#todo create a dictionary  categories={categoryname: merchcode} create a menu funtion?
+
+'''todo add new transaction variable newPurchase then select from categories menu  then push to purchaseTotals  [if k=[ k for k,v in categories] purchaseTotals.push( '''
+
+
+
