0ce41d4c05ede339db1b10833e9adfcddf1792eb
nic <DominicB@email.com>
2019-10-04 15:38:43 -0500

    hack app add trackspending update user menu and user choice

--- trackSpending.py
+++ trackSpending.py
@@ -1,12 +1,37 @@
+
+from useresapi import user_IDs
+from inUtils import  posPutInt
 #todo get user info
 #todo create menu that chooses user
-from useresapi import user_IDs
+def makeSelection():
+    return posPutInt('please enter a number from 1-5 ')
+def chooseUser():
+    ids=''
+    selection=makeSelection()
+    if  (selection ==1) :
+        ids=user_IDs[0]
+    elif (selection ==2):
+        ids=user_IDs[1]
+    elif (selection ==3):
+        ids=user_IDs[2]
+    elif (selection ==4):
+        ids=user_IDs[3]
+    elif (selection ==5):
+        ids=user_IDs[4]
+    else:
+        print('please enter a valid selection')
+        makeSelection()
+
+    return ids
+if __name__ == '__main__':
+    ids=chooseUser()
+    print(ids)
+    print(user_IDs["Codes"])
 # todo get  userID
-name=input('pleasae enter userid')
-def getUserId(name):
 
 
-# todo get user account info
+
+#todo get user account info
 
 
 
