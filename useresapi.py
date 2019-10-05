a973dacb108961c176c6fa1e4e678bd253fbc5aa
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2019-10-04 20:07:11 -0500

    Sorting fun is being fixed.

--- useresapi.py
+++ useresapi.py
@@ -71,7 +71,6 @@ for i in range(len(transactions)):
     effectiveDateDict[i] = transactions[i][1]
     primaryIDDict[i] = transactions[i][2]
 tempSortedDateList = sorted(effectiveDateDict.items(), key=operator.itemgetter(1))
-print(tempSortedDateList)
 
 tempSortedDateDict = {}
 tempSortedAmountDict = {}
@@ -84,7 +83,18 @@ for i in range(len(tempSortedDateList)):
     tempIDValue = primaryIDDict[index]
     tempSortedAmountDict[index] = tempAmountValue
     tempSortedIDDict[index] = tempIDValue
-    tempSortedDateDict[index] = tempSortedDateList[index]
+    tempSortedDateDict[index] = tempSortedDateList[index] #TODO FIX BAD CODE not grabbing by index
+
+#TODO Convert to clean list and back to dicts
+tempSortedDateList = []
+for item in tempSortedDateDict:
+    tempSortedDateList.append(tempSortedDateDict[item][1])
+ultraTempSortedDateDict = {}
+index = 0
+for item in tempSortedDateDict:
+    ultraTempSortedDateDict[item] = tempSortedDateList[index]
+    index += 1
+tempSortedDateDict = ultraTempSortedDateDict
 
 tempSortedIDList = sorted(tempSortedIDDict.items(), key=operator.itemgetter(1))
 
@@ -99,14 +109,25 @@ for i in range(len(tempSortedIDList)):
     tempAmountValue = tempSortedAmountDict[index]
     sortedDateDict[index] = tempDateValue
     sortedAmountDict[index] = tempAmountValue
-    sortedIDDict[index] = tempSortedIDList[index]
+    sortedIDDict[index] = tempSortedIDList[index] #TODO FIX BAD CODE not grabbing by index
+print(sortedIDDict)
+print()
+# print(sortedAmountDict)
+sortedAmountList = []
+total = 0.0
+for item in sortedAmountDict:
+    sortedAmountList.append(sortedAmountDict[item])
+    total += float(sortedAmountDict[item])
+sortedDateList = []
+for item in sortedDateDict:
+    sortedDateList.append(sortedDateDict[item][1])
+sortedIDList = []
+for item in sortedIDDict:
+    sortedIDList.append(sortedIDDict[item][1])
 
 sortedTransactions = []
-
 for i in range(len(sortedAmountDict)):
-    sortedTransactions.append([sortedAmountDict[i][1], sortedDateDict[i][1], sortedIDDict[i][1]])
-
-print(sortedTransactions)
+    sortedTransactions.append([sortedAmountList[i], sortedDateList[i], sortedIDList[i]])
 
 #TODO count how many clients
 #TODO break each client into lists
@@ -114,26 +135,45 @@ print(sortedTransactions)
 #TODO count for each week
 #TODO return to grand total list
 
-temporaryID = transactions[0][2]
+#TODO REAL TIME grab totls for each week for each client and put them in a dict
+
+print(sortedTransactions)
+
+temporaryID = sortedTransactions[0][2]
 temporaryTotal = 0.0
 totalDict = {}
 
-for transaction in transactions:
-    if transaction[2] != temporaryID:
-        totalDict[temporaryID] = temporaryTotal
-        temporaryID = transaction[2]
-        temporaryTotal = 0.0
-        #print(transaction)
-    temporaryTotal += float(transaction[0])
-totalDict[temporaryID] = temporaryTotal
+# for transaction in sortedTransactions:
+#     if transaction[2] != temporaryID:
+#         totalDict[temporaryID] = temporaryTotal
+#         print(temporaryTotal)
+#         temporaryID = transaction[2]
+#         temporaryTotal = 0.0
+#     temporaryTotal += float(transaction[0])
+# totalDict[temporaryID] = temporaryTotal
+#
+# print(totalDict)
+
+# temporaryID = transactions[0][2]
+# temporaryTotal = 0.0
+# totalDict = {}
+#
+# for transaction in transactions:
+#     if transaction[2] != temporaryID:
+#         totalDict[temporaryID] = temporaryTotal
+#         temporaryID = transaction[2]
+#         temporaryTotal = 0.0
+#         #print(transaction)
+#     temporaryTotal += float(transaction[0])
+# totalDict[temporaryID] = temporaryTotal
 
 
-for total in totalDict:
-    #TODO determine how many weeks the user has been using this to save and multiply it by .002
-    percentToSave = .002
-    print('User: ' + total)
-    amount = float(totalDict[total]) * percentToSave
-    print('Amount to save: $' + str(round(amount, 2)))
+# for total in totalDict:
+#     #TODO determine how many weeks the user has been using this to save and multiply it by .002
+#     percentToSave = .002
+#     print('User: ' + total)
+#     amount = float(totalDict[total]) * percentToSave
+#     print('Amount to save: $' + str(round(amount, 2)))
 
 
 
